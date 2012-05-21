7e3ed7d3ab72b2528c00f5dec0c74a36bf88a8a5
jonathan <jonathan@jonathan-HP-Compaq-6735b.(none)>
2012-05-20 22:16:17 -0600

    wtf

--- tasks/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Tasks::Application.config.secret_token = '26d092a8a3d06328d17257dd2e35c36e18953f33d59696307bacfad8e3f3484872d85ca79b40fc8f086e3e66cecb285d86da2d0df093d4269c432339e05d8dec'
