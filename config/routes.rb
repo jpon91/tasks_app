d5518e4a82c23abe4284d2e42d6fdaa5f576f5d7
Rene Peña <renepena@ubuntu.(none)>
2012-05-23 11:35:46 -0600

    usuarios best

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Tasks::Application.routes.draw do
 
   resources :users
 
-		resources :tasks
+resources :tasks
+resources :sessions
 root to: "tasks#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
